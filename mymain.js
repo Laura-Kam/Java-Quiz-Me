29cd99edbd9f4272729bbd4c5b8053f0bbb74f56
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-07-27 12:34:43 +0100

    added variable with questions

--- mymain.js
+++ mymain.js
@@ -1,9 +1,14 @@
-console.log("hi here");
+
 var startQuiz = document.getElementById("startQuiz");
 var index;
 var score;
 var time;
 var timer;
+var questions;
+var submit = document.getElementById("submit-btn");
+var quizContainer = document.getElementById('quiz-container');
+
+generateQuiz(questions, quizContainer, submit);
 
 startQuiz.addEventListener("click", hideHomeContainer);
 
@@ -14,3 +19,26 @@ function hideHomeContainer() {
     homeContainer.style.display = "none";
   }
 }
+
+var questions = [
+	{
+		question: "The condition in an if/else statement is enclosed with",
+		answers: {
+			a: 'commas',
+			b: 'curly brackets',
+			c: 'quotes'
+      d: 'parenthesis'
+		},
+		correctAnswer: 'd'
+	},
+	{
+		question: "Arrays in Javascript can be used to store",
+		answers: {
+			a: 'numbers and strings',
+			b: 'other arrays',
+			c: 'booleans'
+      d: 'all of the above'
+		},
+		correctAnswer: 'd'
+	}
+];
