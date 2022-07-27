e612cf3191a38fa1fac8b3478fac965f0e8821e0
Laura Kambanis <laurakambanis@Lauras-MacBook-Air-3.local>
2022-07-27 12:06:49 +0100

    creating question objects

--- mymain.js
+++ mymain.js
@@ -5,85 +5,12 @@ var score;
 var time;
 var timer;
 
-startQuiz.addEventListener("click, hideHomeContainer") = 
+startQuiz.addEventListener("click", hideHomeContainer);
 
 function hideHomeContainer() {
   console.log("clicked show question");
   var homeContainer = document.getElementById("home");
   if (homeContainer.style.display !== "none") {
     homeContainer.style.display = "none";
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-/* startQuiz.addEventListener("click", showQuestion);
-
-function showQuestion() {
-  console.log("clicked show question");
-  var homeDiv = document.getElementById("home");
-  homeDiv.style.display = "none";
-}
-
-//targets question
-var question = document.querySelector("question");
-
-var nextButton = document.getElementById("next-button");
-
-//targets the array (list) of options
-var choices = Array.from(document.querySelectorAll(".btn-option"));
-
-//targets the score
-var scoreText = document.querySelector("#score");
-
-let presentQuestion = {};
-let CorrectAnswer = true;
-let score = 0;
-let questionNumber = 0;
-let possibleQuestions = {};
-
-let questions = [
-  {
-    question: "Commonly used data types do NOT include",
-    option1: "strings",
-    option2: "booleans",
-    option3: "alerts",
-    option4: "numbers",
-    answer: 3,
-  },
-  {
-    question: "The condition in an if/else statement is enclosed with",
-    option1: "commas",
-    option2: "curly brackets",
-    option3: "quotes",
-    option4: "parenthesis",
-    answer: 3,
-  },
-  {
-    question: "Arrays in JavaScript can be used to store",
-    option1: "numbers and strings",
-    option2: "other arrays",
-    option3: "booleans",
-    option4: "all of the above",
-    answer: 4,
-  },
-  {
-    question:
-      "A very useful tool used during development and debugging for printing content to the debugger is",
-    option1: "JavaScript",
-    option2: "Terminal/bash",
-    option3: "for loops",
-    option4: "console.log",
-    answer: 4,
-  },
-];
-
-*/
